af2ed62969ca4eaa5cc2aef1939b0761e5eeaab5
Matteo Robin <120198629+5h1ro0o@users.noreply.github.com>
2024-12-06 19:59:02 +0100

    fix

--- functions.py
+++ functions.py
@@ -16,39 +16,30 @@ def matrix(rows, cols):
             matrix[i].append(0)
     return matrix
 
-class Matrix : 
-    def __init__(self):
-        self.rows = 0
-        self.cols = 0
+class Matrix:
+    def __init__(self, rows, cols):
+        self._rows = rows
+        self._cols = cols
+        self._matrix = [[0 for _ in range(cols)] for _ in range(rows)]
 
-    def create(self, rows, cols):
-        self.rows = rows
-        self.cols = cols
-        self.matrix = []
-        for i in range(rows):
-            self.matrix.append([])
-            for j in range(cols):
-                self.matrix[i].append(0)
-        return self.matrix
-    
     def get_value(self, row, col):
-        if 0 <= row < self.rows and 0 <= col < self.cols:
-            return self.matrix[row][col]
+        if 0 <= row < self._rows and 0 <= col < self._cols:
+            return self._matrix[row][col]
         else:
             raise IndexError("les valeurs ne sont pas comprises dans la matrice")
-        
+
     def __eq__(self, other):
-        if self.rows != other.rows or self.cols != other.cols:
+        if self._rows != other._rows or self._cols != other._cols:
             return False
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if self.matrix[i][j] != other.matrix[i][j]:
+        for i in range(self._rows):
+            for j in range(self._cols):
+                if self._matrix[i][j] != other._matrix[i][j]:
                     return False
         return True
     
 
 
-def sorting_list(my_list: [int]) -> [int]:
+def my_sort(my_list: [int]) -> [int]:
     sorted_list = my_list[:]
     n = len(sorted_list)
     for i in range(n):
